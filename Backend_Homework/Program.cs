1350e9f415499b1de0ee7cc88af24b0653441c44
jindra12.underdark <jindra12.underdark@gmail.com>
2023-03-30 21:00:30 +0200

    Create comments on how bad the program is

--- Backend_Homework/Program.cs
+++ Backend_Homework/Program.cs
@@ -12,26 +12,31 @@ namespace Continero.Homework
     }
     class Program
     {
+        /// <summary>
+        /// This way of programming is not extensible, you cannot easily add formats to process
+        /// </summary>
         static void Main(string[] args)
         {
+            // Does not accept parameters from args
             var sourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\Document1.xml");
 
 
             var targetFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Target Files\\Document1.json");
-        try
+        try // bad formatting
             {
+                // Does not check if file is missing
                 FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
-                var reader = new StreamReader(sourceStream);
+                var reader = new StreamReader(sourceStream); // no using() syntax, might leave the file open
                 string input = reader.ReadToEnd();
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                throw new Exception(ex.Message); // Does not make sense to consume an exception, do nothing and then re-throw a message
             }
             var xdoc = XDocument.Parse(input);
             var doc = new Document
             {
-                Title = xdoc.Root.Element("title").Value,
+                Title = xdoc.Root.Element("title").Value, // Does not check if the elements are present
                 Text = xdoc.Root.Element("text").Value
             };
             var serializedDoc = JsonConvert.SerializeObject(doc);
