e7f0b45b0903297ea4c5eb9fa19c334c8e26c99f
jindra12.underdark <jindra12.underdark@gmail.com>
2023-04-03 20:25:34 +0200

    Add comments to command line processor

--- Backend_Homework/Processor/CommandLineProcessor.cs
+++ Backend_Homework/Processor/CommandLineProcessor.cs
@@ -5,39 +5,122 @@ using Backend_Homework.FileManagers;
 
 namespace Backend_Homework.Processor
 {
+    /// <summary>
+    /// Builds a conversion with saving and loading through console commands
+    /// </summary>
     public class CommandLineProcessor
     {
+        /// <summary>
+        /// Map of possible types of converters, using CommandLineAttribute value as index
+        /// </summary>
         private readonly Lazy<IDictionary<string, Type>> converters = new Lazy<IDictionary<string, Type>>(() => LoadTypesFromAssembly(typeof(IConverter)));
+        /// <summary>
+        /// Map of possible types of file loaders, using CommandLineAttribute value as index
+        /// </summary>
         private readonly Lazy<IDictionary<string, Type>> fileManagers = new Lazy<IDictionary<string, Type>>(() => LoadTypesFromAssembly(typeof(IFileManager)));
 
+        /// <summary>
+        /// Converter to be applied to input
+        /// </summary>
         private IConverter? inConverter;
+        
+        /// <summary>
+        /// Converter to be applied to output
+        /// </summary>
         private IConverter? outConverter;
+        
+        /// <summary>
+        /// Input file manager
+        /// </summary>
         private IFileManager? inFileManager;
+        
+        /// <summary>
+        /// Output file manager
+        /// </summary>
         private IFileManager? outFileManager;
+        
+        /// <summary>
+        /// Configuration for input file manager
+        /// </summary>
         private string? inFileManagerConfig;
+
+        /// <summary>
+        /// Configuration for output file manager
+        /// </summary>
         private string? outFileManagerConfig;
 
+        /// <summary>
+        /// Describes names of console arguments
+        /// </summary>
         private struct ArgumentTypes
         {
+            /// <summary>
+            /// In format argument name
+            /// </summary>
             public const string InFormat = "-in";
+            /// <summary>
+            /// Out format argument name
+            /// </summary>
             public const string OutFormat = "-out";
+            /// <summary>
+            /// Input file configuration
+            /// </summary>
             public const string InFile = "-from";
+            /// <summary>
+            /// Output file configuration
+            /// </summary>
             public const string OutFile = "-to";
         }
 
+        /// <summary>
+        /// States of builder from console args
+        /// </summary>
         private enum ProcessorState
         {
+            /// <summary>
+            /// Is expecting one of argument types
+            /// </summary>
             ExpectingConfiguration,
+    
+            /// <summary>
+            /// Expects the next argument to be input format for conversion
+            /// </summary>
             ExpectingInFormat,
+    
+            /// <summary>
+            /// Expects the next argument to be output format for conversion
+            /// </summary>
             ExpectingOutFormat,
+
+            /// <summary>
+            /// Expect the next argument to be type of file manager
+            /// </summary>
             ExpectingInFileManager,
+
+            /// <summary>
+            /// Expect the next argument to be type of file manager
+            /// </summary>
             ExpectingOutFileManager,
+
+            /// <summary>
+            /// Expects the next argument to be file manager input configuration
+            /// </summary>
             ExpectingInFileManagerConfig,
+
+            /// <summary>
+            /// Expects the next argument to be file manager output configuration
+            /// </summary>
             ExpectingOutFileManagerConfig,
         }
 
+        /// <summary>
+        /// Current state of processor
+        /// </summary>
         private ProcessorState state = ProcessorState.ExpectingConfiguration;
 
+        /// <summary>
+        /// Processes a single argument from console
+        /// </summary>
         public CommandLineProcessor ProcessArgument(string argument)
         {
             if (TryParseCommandState(argument, out var nextState))
@@ -81,6 +164,9 @@ namespace Backend_Homework.Processor
             return this;
         }
 
+        /// <summary>
+        /// After processing arguments, run this method to convert files
+        /// </summary>
         public async Task Run()
         {
             if (inConverter is null || outConverter is null || outFileManager is null || inFileManager is null || inFileManagerConfig is null || outFileManager is null || outFileManagerConfig is null)
@@ -96,6 +182,12 @@ namespace Backend_Homework.Processor
             }
         }
 
+        /// <summary>
+        /// Tries to parse next state from argument types
+        /// </summary>
+        /// <param name="argument">argument from console line</param>
+        /// <param name="nextState">Will be null if argument does not match one of ArgumentTypes struct const values</param>
+        /// <returns>True, if argument matches one of ArgumentTypes</returns>
         private bool TryParseCommandState(string argument, [NotNullWhen(true)] out ProcessorState? nextState)
         {
             switch (argument)
@@ -118,6 +210,10 @@ namespace Backend_Homework.Processor
             }
         }
 
+        /// <summary>
+        /// Matches a type with console line argument
+        /// </summary>
+        /// <typeparam name="T">An instance of interface T matching the string argument through an attribute</typeparam>
         private T GetInstanceFromCommandLine<T>(IDictionary<string, Type> types, string argument) where T : class
         {
             if (!types.ContainsKey(argument))
@@ -125,6 +221,11 @@ namespace Backend_Homework.Processor
             return Activator.CreateInstance(types[argument]) as T ?? throw new InvalidOperationException($"Cannot create class of type {nameof(T)}");
         }
 
+        /// <summary>
+        /// Loads types from current assembly that match specific Type and have the CommandLine Attribute
+        /// </summary>
+        /// <param name="baseType">Interface type to match from current assembly</param>
+        /// <returns>A map of types matching the Type from parameter</returns>
         private static IDictionary<string, Type> LoadTypesFromAssembly(Type baseType)
         {
             return AppDomain
